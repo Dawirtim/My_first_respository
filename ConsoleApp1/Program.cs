e87162f320613f791e67077ad891bd1d6c8cbb7e
Dawid Mieczkowski <dawid.mieczkowski@plum.pl>
2025-09-24 09:36:19 +0200

    Added Skoda Superb

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,13 @@
 ﻿namespace respository;
+using system.data.SqlClient;
 internal class Program
 {
     private static void Main(string[] args)
     {
         Samochod mercedesc180cdiw204 = new Samochod("Dark Red", "Mercedes", "Klasa C", 2112, 4);
         mercedesc180cdiw204.Opis();
+        Samochod skodaSuperb = new Samochod("Dark Green", "Skoda", "Superb", 1996, 4);
+        skodaSuperb.Opis();
         Console.ReadKey();
     }
 }
