814a7dee066f8bbcd797d7bf69081bdc6fdd9538
Dawid Mieczkowski <dawid.mieczkowski@plum.pl>
2025-09-24 11:02:12 +0200

    Added Bmw M5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,8 @@ internal class Program
         renaulttalisman.Opis();
         Samochod fordfocus = new Samochod("Yellow", "Ford", "Focus", 1545, 2);
         fordfocus.Opis();
+        Samochod bmwM5 = new Samochod("Lime", "Bmw", "M5", 3546, 4);
+        bmwM5.Opis();
         Console.ReadKey();
     }
 }
