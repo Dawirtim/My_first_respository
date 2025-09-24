73ceb4f56f66c1dc550c4b3b2f543f06754b4413
Dawid Mieczkowski <dawid.mieczkowski@plum.pl>
2025-09-24 08:57:00 +0200

    Second commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿namespace respository;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Samochod mercedesc180cdiw204 = new Samochod("Dark Red", "Mercedes", "Klasa C", 2112, 4);
+        mercedesc180cdiw204.Opis();
+        Console.ReadKey();
     }
 }
---
+++ ConsoleApp1/Samochód.cs
@@ -0,0 +1,20 @@
+﻿namespace respository
+{
+    public class Samochod
+    {
+        string kolor, marka, model;
+        int pojemnoscSilnika, iloscDrzwi;
+        public Samochod(string kolor, string marka, string model, int pojemnoscSilnika, int iloscDrzwi)
+        {
+            this.kolor = kolor;
+            this.marka = marka;
+            this.model = model;
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.iloscDrzwi = iloscDrzwi;
+        }
+        public void Opis()
+        {
+            Console.WriteLine($"Kolor: {kolor}, Marka: {marka}, Pojemność silnika {pojemnoscSilnika} cm^3, Ilosc drzwi: {iloscDrzwi}");
+        }
+    }
+}
